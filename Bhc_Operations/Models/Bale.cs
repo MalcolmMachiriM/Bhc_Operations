1221d51c66b4aac220c72a2671033f3125b34482
Malcolm <machirimalcolm@gmail.com>
2023-06-12 22:59:33 +0200

    reworks

--- Bhc_Operations/Models/Bale.cs
+++ Bhc_Operations/Models/Bale.cs
@@ -78,8 +78,87 @@ namespace Bhc_Operations.Models
         {
             return CalculateDebt(debt, gross, time) + (10.02f * Mass * Price);
         }
+
+
         #endregion
     }
 
-    
+    public class Program
+    {
+        public static void Main()
+        {
+            // Example usage
+            List<Bale> bales = new List<Bale>
+        {
+            new Bale { Barcode = "110000013", Mass = 50, Grade = "TMOS", Price = 10 },
+            new Bale { Barcode = "110000012", Mass = 40, Grade = "TMOS", Price = 12 },
+            new Bale { Barcode = "110000011", Mass = 30, Grade = "TLOS", Price = 8 },
+            new Bale { Barcode = "110000010", Mass = 50, Grade = "TLOS", Price = 10 },
+            new Bale { Barcode = "110000014", Mass = 80, Grade = "TPOS", Price = 15 }
+        };
+
+            reworks(bales);
+        }
+
+        public static void reworks(List<Bale> bales)
+        {
+            List<Bale> mergedBales = new List<Bale>();
+            List<Bale> rejectedBales = new List<Bale>();
+            double totalMass = 0;
+            double totalGross = 0;
+
+            // Sort the bales by grade in ascending order
+            bales = bales.OrderBy(bale => bale.Grade).ToList();
+
+            foreach (Bale bale in bales)
+            {
+                if (totalMass + bale.Mass <= 120)
+                {
+                    // Check if there is a bale with the same grade to merge
+                    Bale mergedBale = mergedBales.FirstOrDefault(m => m.Grade == bale.Grade);
+                    if (mergedBale != null)
+                    {
+                        // Merge the bales
+                        mergedBale.Mass += bale.Mass;
+                        mergedBale.Barcode += "+" + bale.Barcode;
+                        totalMass += bale.Mass;
+                        totalGross += bale.Mass * bale.Price;
+                    }
+                    else
+                    {
+                        // Add the bale to the merged bales
+                        mergedBales.Add(bale);
+                        totalMass += bale.Mass;
+                        totalGross += bale.Mass * bale.Price;
+                    }
+                }
+                else
+                {
+                    // Add the bale to the rejected bales
+                    rejectedBales.Add(bale);
+                }
+            }
+
+            // Print the merged bales
+            Console.WriteLine("Merged Bales:");
+            foreach (Bale mergedBale in mergedBales)
+            {
+                Console.WriteLine($"Barcode: {mergedBale.Barcode}, Mass: {mergedBale.Mass}, Grade: {mergedBale.Grade}, Price: {mergedBale.Price}");
+            }
+
+            // Print the rejected bales
+            Console.WriteLine("Rejected Bales:");
+            foreach (Bale rejectedBale in rejectedBales)
+            {
+                Console.WriteLine($"Barcode: {rejectedBale.Barcode}, Mass: {rejectedBale.Mass}, Grade: {rejectedBale.Grade}, Price: {rejectedBale.Price}");
+            }
+
+            // Calculate and print the new average price for the merged bales
+            double averagePrice = totalGross / totalMass;
+            Console.WriteLine($"New Average Price: {averagePrice}");
+        }
+    }
+
+
+
 }
