8aaf900223806221d5964416d765362a13a8b6b2
Malcolm <machirimalcolm@gmail.com>
2023-06-12 07:16:13 +0200

    pallete model

---
+++ Bhc_Operations/Models/Pallete.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bhc_Operations.Models
+{
+    public class Pallete
+    {
+        #region variables
+        protected string mpalleteNumber;
+        protected List<Bale> mbales;
+        protected string mwarehouse;
+        protected List<string> mgrade;
+        #endregion
+
+        #region properties
+        public string palleteNumber { get { return mpalleteNumber; } set {  mpalleteNumber = value; } }
+        public List<Bale> bales { get { return mbales; } set {  mbales = value; } }
+        public string warehouse { get { return warehouse; } set {  warehouse = value; } }
+        public string grade { get {  return grade; } set {  grade = value; } }
+        #endregion
+    }
+}
